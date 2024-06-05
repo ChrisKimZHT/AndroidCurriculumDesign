1d0af72971cfcea7c5b93e0ff5a87e77dcae2b33
ChrisKim <1515748548@qq.com>
2024-06-05 15:54:00 +0800

    [feat] 笔记列表

--- app/src/main/java/com/zouht/note/activity/MainActivity.kt
+++ app/src/main/java/com/zouht/note/activity/MainActivity.kt
@@ -2,15 +2,26 @@ package com.zouht.note.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zouht.note.R
+import com.zouht.note.data.NoteManager
+import com.zouht.note.model.Note
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private val noteManager = NoteManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         initListeners()
+        initNoteList()
     }
 
     override fun onStart() {
@@ -46,6 +58,17 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.logoutBtn).setOnClickListener { onLogoutBtnClick() }
     }
 
+    private fun initNoteList() {
+//        val notes = noteManager.listNotes()
+        val notes = ArrayList<Note>()
+        for (i in 1..100) {
+            notes.add(Note(i, 1, "test$i", "content", 0))
+        }
+        val noteList = findViewById<RecyclerView>(R.id.noteList)
+        noteList.layoutManager = LinearLayoutManager(this)
+        noteList.adapter = NoteListAdapter(notes)
+    }
+
     private fun onLogoutBtnClick() {
         val sharedPref = getSharedPreferences("login", MODE_PRIVATE)
         val editor = sharedPref.edit()
@@ -54,4 +77,26 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
+}
+
+class NoteListAdapter(private val notes: List<Note>) :
+    RecyclerView.Adapter<NoteListAdapter.ViewHolder>() {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val noteTitle: TextView = view.findViewById(R.id.noteTitle)
+        val noteTime: TextView = view.findViewById(R.id.noteTime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.note_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+        holder.noteTitle.text = notes[position].title
+        holder.noteTime.text = sdf.format(notes[position].createdTime)
+    }
+
+    override fun getItemCount() = notes.size
 }
