c39e09f12a3fbdf8a63f8f5ea66598db85c82cc0
ChrisKim <1515748548@qq.com>
2024-06-05 16:26:58 +0800

    [feat] 编辑笔记功能

--- app/src/main/java/com/zouht/note/activity/EditActivity.kt
+++ app/src/main/java/com/zouht/note/activity/EditActivity.kt
@@ -1,16 +1,21 @@
 package com.zouht.note.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.zouht.note.R
+import com.zouht.note.data.NoteManager
+import com.zouht.note.model.Note
 
 class EditActivity : AppCompatActivity() {
     private var noteId = -1;
+    private val noteManager = NoteManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +26,59 @@ class EditActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initNoteId()
+        initActionBar()
+        initListeners()
+        initNote()
     }
 
-    private fun initNoteId() {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initActionBar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun initNote() {
         noteId = intent.getIntExtra("noteId", -1)
         if (noteId == -1) {
             findViewById<Toolbar>(R.id.toolbar).title = "新建笔记"
             findViewById<Button>(R.id.save).text = "新建"
+        } else {
+            findViewById<Toolbar>(R.id.toolbar).title = "编辑笔记"
+            findViewById<Button>(R.id.save).text = "保存"
+            val note = noteManager.getNoteByNoteId(noteId)
+            if (note == null) {
+                finish()
+                return
+            }
+            findViewById<EditText>(R.id.title).setText(note.title)
+            findViewById<EditText>(R.id.content).setText(note.content)
         }
     }
 
+    private fun initListeners() {
+        findViewById<Button>(R.id.save).setOnClickListener { onSaveBtnClick() }
+    }
 
+    private fun onSaveBtnClick() {
+        val title = findViewById<EditText>(R.id.title).text.toString()
+        val content = findViewById<EditText>(R.id.content).text.toString()
+        val userId = getSharedPreferences("login", MODE_PRIVATE).getInt("userId", -1)
+        val createdTime = System.currentTimeMillis()
+        val note = Note(noteId, userId, title, content, createdTime)
+        if (noteId == -1) {
+            noteManager.insertNote(note)
+        } else {
+            noteManager.updateNote(note)
+        }
+        finish()
+    }
 }
