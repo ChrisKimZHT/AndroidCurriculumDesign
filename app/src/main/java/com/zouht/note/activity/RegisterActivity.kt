a359edd2030ad0e8fcb78b258f2443d4a874ba86
ChrisKim <1515748548@qq.com>
2024-06-05 13:57:33 +0800

    [refactor] onBackPressed换成finish

--- app/src/main/java/com/zouht/note/activity/RegisterActivity.kt
+++ app/src/main/java/com/zouht/note/activity/RegisterActivity.kt
@@ -27,7 +27,7 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
-            onBackPressed()
+            finish()
             return true
         }
         return super.onOptionsItemSelected(item)
